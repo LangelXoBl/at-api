cb0552d26d11355bf9967e2d3ba874b4da30bfff
Luis Xool <langelxobl@gmail.com>
2024-03-25 13:11:56 -0500

    add tests by example

--- src/example/infrastructure/example.controller.spec.ts
+++ src/example/infrastructure/example.controller.spec.ts
@@ -15,8 +15,34 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const res = await appController.getHello();
+
+      expect(res).toBe('Hello World!');
+    });
+  });
+
+  describe('createHello1', () => {
+    it('should return "Hello John Doe"', async () => {
+      const res = await appController.createHello({ name: 'John Doe' });
+
+      expect(res).toBe('Hello John Doe');
+    });
+  });
+  // Add more tests by createHello method
+  describe('createHello2', () => {
+    it('should return "Hello foo"', async () => {
+      const res = await appController.createHello({ name: 'foo' });
+
+      expect(res).toBe('Hello foo');
+    });
+  });
+
+  describe('createHello3', () => {
+    it('should return "Hello dao"', async () => {
+      const res = await appController.createHello({ name: 'dao' });
+
+      expect(res).toBe('Hello dao');
     });
   });
 });
